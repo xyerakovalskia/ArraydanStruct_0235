584f9eb0138b549e901c216d0e46866205a47a26
xyerakovalskia <xyera123987@gmail.com>
2026-04-06 19:09:10 +0700

    arraystruct_3

--- arraystruct.cpp
+++ arraystruct.cpp
@@ -13,3 +13,34 @@ struct Orang
     DetailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs[3];
+    
+    for (int i = 0; i <= 2; i++)
+    {
+
+    // mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : ";
+    getline(cin ,  mhs[i].nama);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.desa);
+    cout << "Alamat : ";
+    getline(cin , mhs[i].alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs[i].umur;
+    cout << endl;
+
+    }
+
+    for (int i = 0; i <= 2; i++)
+    {
+    // tampilkan
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs[i].nama << endl;
+    cout << "Desa : " << mhs[i].alamat.desa << endl;
+    cout << "Kota : " << mhs[i].alamat.kota << endl;
+    cout << "Umur : " << mhs[i].umur << endl;
+    }
+}
