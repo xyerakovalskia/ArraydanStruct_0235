b3fa565858d524a338015d3829496c8cc09c4da1
xyerakovalskia <xyera123987@gmail.com>
2026-04-06 18:57:31 +0700

    struct2_3

--- struct2.cpp
+++ struct2.cpp
@@ -11,3 +11,25 @@ struct Orang
     detailAlamat alamat;
     int umur;
 };
+
+int main () {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : " ;
+    cin >> mhs.nama;
+    cout << "desa : " ;
+    cin >> mhs.alamat.desa;
+    cout << "kota : " ;
+    cin >> mhs.alamat.kota;
+    cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+    // Menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+
+}
