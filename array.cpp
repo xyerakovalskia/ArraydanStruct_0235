aac7766b25c5c81ce4ccedac3255a1ff29c1146c
xyerakovalskia <xyera123987@gmail.com>
2026-04-06 18:52:08 +0700

    array_2

--- array.cpp
+++ array.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku[5] = {10,20,30,40,50};
+    // deklarasi array tanpa inisialisasi
+    int kamu[5];
+    // tampilkan data(membaca data array)
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    // ganti index ke 1(menulis data array)
+    aku[1] = 200;
+    cout << endl;
+    cout << "Data pada index 1 = " << aku[1] << endl;
+    // Mengisi Satu per Satu
+          cout << "Data index 0 = ";
+          cin >> kamu[0];
+          cout << "Data index 1 = ";
+          cin >> kamu[1];
+          cout << "Data index 2 = ";
+          cin >> kamu[2];
+          cout << "Data index 3 = ";
+          cin >> kamu[3];
+          cout << "Data index 4 = ";
+          cin >> kamu[4];
+          cout << endl;
+          // Menampilkan Satu per Satu
+          cout << "Data Pertama = " << kamu[0] << endl;
+          cout << "Data Kedua = " << kamu[1] << endl;
+          cout << "Data Ketiga = " << kamu[2] << endl;
+          cout << "Data Keempat = " << kamu[3] << endl;
