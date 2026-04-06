1086e0a03906180c3694a2531d8ef5b1cdc09e4b
xyerakovalskia <xyera123987@gmail.com>
2026-04-06 18:51:13 +0700

    first commit

---
+++ getline.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+struct Orang 
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main () {
+    Orang mhs;
+    // Mengisi data
+    cout << "Mengisi Data" << endl;
+    cout << "Nama : " ;
+    getline(cin, mhs.nama);
+    cout << "desa : " ;
+    getline(cin, mhs.alamat.desa);
+    cout << "kota : " ;
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : " ;
+    cin >> mhs.umur;
+    cout << endl;
+    // Menampilkan data
+    cout << "Menampilkan Data" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "desa : " << mhs.alamat.desa << endl;
+    cout << "kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+
+}
