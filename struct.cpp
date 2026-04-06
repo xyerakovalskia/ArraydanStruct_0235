db96bf9769c393a54725f184951bc3c7a4fdac0f
xyerakovalskia <xyera123987@gmail.com>
2026-04-06 18:53:13 +0700

    struct_1

--- struct.cpp
+++ struct.cpp
@@ -1,27 +1,2 @@
 #include <iostream>
 using namespace std;
-
-struct Orang
-{
-    string nama;
-    string alamat;
-    int umur;
-};
-
-int main() {
-    Orang mhs;
-    // Mengisi data
-    cout << "Mengisi Data" << endl;
-    cout << "Nama : ";
-    cin >> mhs.nama;
-    cout << "Alamat : ";
-    cin >> mhs.alamat;
-    cout << "Umur : ";
-    cin >> mhs.umur;
-    cout << endl;
-    // tampilkan
-    cout << "Menampilkan Data" << endl;
-    cout << " Nama : " << mhs.nama << endl;
-    cout << " Alamat : " << mhs.alamat << endl;
-    cout << " Umur : " << mhs.umur << endl;
-}
